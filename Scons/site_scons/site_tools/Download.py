a7d62b727911f5e0d67f8712667268f14e797090
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-04-06 02:22:10 +0200

    adding Scons download builder

---
+++ Scons/site_scons/site_tools/Download.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import urllib2
+import urlparse
+
+import SCons.Builder
+import SCons.Node
+
+
+
+# creates the downloading output message
+# @param s original message
+# @param target target name
+# @param source source name
+# @param env environment object
+def __message( s, target, source, env ) : 
+    print "downloading [%s] to [%s] ..." % (source[0], target[0])
+
+
+# the download function, which reads the data from the URL
+# and writes it down to the file
+# @param target target file on the local drive
+# @param source URL for download
+# @env environment object
+def __action( target, source, env ) :
+    try :
+        stream = urllib2.urlopen( str(source[0]) )
+        file   = open( str(target[0]), "wb" )
+        file.write(stream.read())
+        file.close()
+        stream.close()
+    except Exception, e :
+        raise SCons.Errors.StopError( e )
+
+
+# defines the emitter of the builder
+# @param target target file on the local drive
+# @param source URL for download
+# @env environment object
+def __emitter( target, source, env ) :
+    if not env["DOWNLOAD_USEURLFILENAME"] :
+        return target, source
+
+    try :
+        url = urlparse.urlparse( str(source[0]) )
+    except Exception, e :
+        raise SCons.Errors.StopError( e )
+
+    return url.path.split("/")[-1], source
+
+
+
+
+# generate function, that adds the builder to the environment,
+# the value "DOWNLOAD_USEFILENAME" replaces the target name with
+# the filename of the URL
+# @env environment object
+def generate( env ):
+    env["BUILDERS"]["Download"] = SCons.Builder.Builder( action = __action,  emitter = __emitter,  target_factory = SCons.Node.FS.File,  source_factory = SCons.Node.Python.Value,  PRINT_CMD_LINE_FUNC = __message )
+    env.Replace(DOWNLOAD_USEURLFILENAME = True)
+
+# existing function of the builder
+# @return true
+def exists(env):
+    return 1
+
+
