3a3e304e11fc612cac27bf206433be0955c6bea2
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-04-07 20:14:42 +0200

    base repository builder

--- Scons/site_scons/site_tools/Repository.py
+++ Scons/site_scons/site_tools/Repository.py
@@ -15,6 +15,32 @@ import SCons.Util
 # setting default parameter
 # @param env environment object
 def __detect( env ) :
+    env["GITPARAMETER"] = ""
+    env["SVNPARAMETER"] = ""
+    
+    env["REPOSITORY"] = {
+    
+        "SVN" : {
+            
+            "RUN"       : "",
+            "PULL"      : "",
+            "PUSH"      : "",
+            "COMMIT"    : "",
+            
+        },
+        
+        "GIT" : {
+            
+            "RUN"       : "git",
+            "PULL"      : "${REPOSITORY['GIT']['RUN']} $GITPARAMETER --work-tree=$TARGET pull",
+            "PUSH"      : "${REPOSITORY['GIT']['RUN']} $GITPARAMETER --work-tree=$TARGET push",
+            "CLONE"     : "${REPOSITORY['GIT']['RUN']} $GITPARAMETER clone $SOURCE $TARGET",
+            "COMMIT"    : "${REPOSITORY['GIT']['RUN']} $GITPARAMETER --work-tree=$SOURCE add . && ${REPOSITORY['GIT']['RUN']} $GITPARAMETER --work-tree=$SOURCE commit -m $TARGET"
+            
+        }
+    
+    }
+
     pass
 
 
@@ -23,14 +49,17 @@ def __detect( env ) :
 # @param target target name
 # @param source source name
 # @param env environment object
-def __message( s, target, source, env ) : 
-    pass
+def __GitCloneMessage( s, target, source, env ) : 
+    print "Git clone [%s] into [%s] ..." % (source[0], target[0])
 
 
 # generate function, that adds the builder to the environment
 # @env environment object
 def generate( env ) :
     __detect(env)
+
+    env["BUILDERS"]["GitClone"] = SCons.Builder.Builder( action = SCons.Action.Action("${REPOSITORY['GIT']['CLONE']}"),  target_factory = SCons.Node.FS.Dir,  source_factory = SCons.Node.Python.Value,  PRINT_CMD_LINE_FUNC = __GitCloneMessage )
+    env["BUILDERS"]["GitPull"] = SCons.Builder.Builder( action = SCons.Action.Action("${REPOSITORY['GIT']['PULL']}"),  target_factory = SCons.Node.Python.Value,  source_factory = SCons.Node.FS.Dir,  PRINT_CMD_LINE_FUNC = __GitCloneMessage )
 
 
 # existing function of the builder
